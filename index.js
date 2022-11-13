0a64aff96083acf767ffc500d6d50c6a53092756
SErgioNbP <sergionevespinheiro@gmail.com>
2022-11-13 00:40:03 +0000

    search for both movies added

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-createAutoComplete({
-    root: document.querySelector('.autocomplete'),
+const autocompleteConfig = {
     renderOption(movie) {
         const imgSrc = movie.Poster === 'N/A' ? 'https://media.istockphoto.com/id/1222357475/vector/image-preview-icon-picture-placeholder-for-website-or-ui-ux-design-vector-illustration.jpg?s=612x612&w=0&k=20&c=KuCo-dRBYV7nz2gbk4J9w1WtTAgpTdznHu55W9FjimE=' : movie.Poster;
         return `
             <img src="${imgSrc}" />
             ${movie.Title} (${movie.Year})
         `;
-    },
-    onOptionSelect(movie) {
-        onMovieSelect(movie);
     },
     inputValue(movie) {
         return movie.Title;
@@ -20,16 +16,33 @@ createAutoComplete({
                 s: searchTerm
             }
         });
-    
+
         if (response.data.Error) {
             return [];
         }
-    
+
         return response.data.Search;
+    }
+}
+
+createAutoComplete({
+    ...autocompleteConfig,
+    root: document.querySelector('#left-autocomplete'),
+    onOptionSelect(movie) {
+        document.querySelector('.tutorial').classList.add('is-hidden');
+        onMovieSelect(movie, document.querySelector('#left-summary'));
+    }
+});
+createAutoComplete({
+    ...autocompleteConfig,
+    root: document.querySelector('#right-autocomplete'),
+    onOptionSelect(movie) {
+        document.querySelector('.tutorial').classList.add('is-hidden');
+        onMovieSelect(movie,  document.querySelector('#right-summary'));
     }
 });
 
-const onMovieSelect = async (movie) => {
+const onMovieSelect = async (movie, summaryElement) => {
     const response = await axios.get('http://www.omdbapi.com/', {
         params: {
             apikey: '15ea7872',
@@ -37,7 +50,7 @@ const onMovieSelect = async (movie) => {
         }
     });
 
-    document.querySelector('#summary').innerHTML = movieTemplate(response.data);
+    summaryElement.innerHTML = movieTemplate(response.data);
 };
 
 const movieTemplate = (movieDetail) => {
